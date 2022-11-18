36fed0eecbab58f77154aa1be854a2b84c1c3a4e
Paul Donovan <pauldonovandonegal@gmail.com>
2022-11-18 18:30:49 +0000

    Updated README

--- auto-render.sh
+++ auto-render.sh
@@ -1,55 +1,96 @@
 #!/bin/bash
 
+eval "$(conda shell.bash hook)"
+conda activate ffmpeg
+
 function render {
-
-	TEMP=$(mktemp /tmp/tmp.XXXXXXXX)	
-	cp "$1" "$TEMP.RPP"
-
-	if [ ! -z "$preview" ]
-	then
-		sed -i.bak 's/RENDER_RANGE.*/RENDER_RANGE 0 0 30 16 1000/' "$TEMP.RPP"
-	fi
-
-	/Applications/REAPER64.app/Contents/MacOS/REAPER -renderproject "$TEMP.RPP"
+	new_basename=$(basename $1) # | sed 's/.RPP//g')
+	#/Applications/REAPER64.app/Contents/MacOS/REAPER -renderproject "$1"
+	/Applications/REAPER.app/Contents/MacOS/REAPER -renderproject "$1" >> reaper.log 2>&1 # This gets rendered as 'Deep Socks.mp3' most of the time 
 }
 
-source="."
-destination="."
-projects="latest"
-preview=""
+function is_this_render_new {
+	my_flag="FALSE"
+	filename=$(basename -- "$1")
+	extension="${filename##*.}"
+	just_filename=$(echo $filename | rev  | cut -d'.' -f2- | rev)
+	filename_rendered_before=$(grep $just_filename /Users/pauldonovan/Tools/reaper-auto-render/FinishedProjects.txt | wc -l)
+	if [ "$filename_rendered_before" -gt 0 ]; then
+		my_flag="TRUE"
+	fi
+}
+
+function find_render {
+	echo "Looking for render for $1"
+	my_render=$(find /Users/pauldonovan/Dropbox/Pauls_crap/ReaperMusic/ -type f -print0 | xargs -0 stat -f "%m %N" | sort -rn | grep -v ".DS_Store" | head -1 | cut -f2- -d" ")
+	echo "Found file: $my_render"
+}
+
+function rename_and_convert_to_mp3 {
+	if [[ "$1" == *"untitled"* ]]; then
+		echo "Untitled track. Error likely occurred, moving on..."
+	else
+		#echo "New filename: $2.mp3"
+		### ffmpeg trims empty space and forces mp3 format
+		ffmpeg -y -f mp3 \
+			-hide_banner -loglevel error \
+		    -i "$1" \
+			-af "silenceremove=start_periods=1:start_duration=1:start_threshold=-60dB:detection=peak,aformat=dblp,areverse,silenceremove=start_periods=1:start_duration=1:start_threshold=-60dB:detection=peak,aformat=dblp,areverse" \
+			"/Users/pauldonovan/tools/reaper-auto-render/$2.mp3" >> ffmpeg.log 2>&1
+		# Check if ffmpeg conversion to mp3 failed. If so, copy original file
+		if [ $? -eq 0 ]; then
+			echo "ffmpeg conversion successful"
+			mv "/Users/pauldonovan/tools/reaper-auto-render/$2.mp3" "$destination/$2.mp3"
+			echo "Moved file here: $destination/$2.mp3"
+		else
+			echo "ffmpeg conversion failed. Retrying using original file format"
+			filename=$(basename -- "$1")
+			extension="${filename##*.}"
+			ffmpeg -y -f $extension \
+				-hide_banner -loglevel error \
+		    	-i "$1" \
+				-af "silenceremove=start_periods=1:start_duration=1:start_threshold=-60dB:detection=peak,aformat=dblp,areverse,silenceremove=start_periods=1:start_duration=1:start_threshold=-60dB:detection=peak,aformat=dblp,areverse" \
+				"/Users/pauldonovan/tools/reaper-auto-render/$2.$extension" >> ffmpeg.log 2>&1
+			mv "/Users/pauldonovan/tools/reaper-auto-render/$2.$extension" "$destination/$2.$extension"
+			rm /Users/pauldonovan/tools/reaper-auto-render/*.mp3
+			echo "Moved file here: $destination/$2.$extension"
+		fi
+	fi
+}
+
+
+source="/Users/pauldonovan/Dropbox/Pauls_crap/ReaperMusic/"
+destination="/Users/pauldonovan/Dropbox/Pauls_crap/ReaperMusic/Renders/BulkRenders/"
 
 while getopts s:d:lap flag
 do
     case "${flag}" in
         s) source=${OPTARG};;
         d) destination=${OPTARG};;
-		l) projects="latest";;
 		a) projects="all" ;;
-		p) preview="true" ;;
     esac
 done
 
 all_projects=""
 
-for i in $(find $source -type f -name '*.RPP' | sed -r 's|/[^/]+$||' |sort |uniq); do
-    
-	if [ $projects = "latest" ]; then
-		result=`ls -t $i/*.RPP | head -1`
-		all_projects="$all_projects\n$result"
-	else
-		result=`ls -1 $i/*.RPP`
-		all_projects="$all_projects\n$result"
-	fi
+for i in $(find $source -type f -name '*.RPP' | sort | uniq); do
+	all_projects="$all_projects\n$i"
 done
 
 project_list=`echo -e "$all_projects"`
 
-while IFS= read -r line; do
-
-	if [ ! -z "$line" ]
-	then
-	      echo "Rendering: $line"
-	      render "$line"
+for line in $project_list
+do
+	echo -e "\nRendering: $line"
+	real_filename=$(basename $line | sed 's/.RPP//g')
+	render "$line"
+	find_render "$line"
+	is_this_render_new "$real_filename"
+	if [ "$my_flag" == "TRUE" ]; then
+		echo -e "This file was already generated, Reaper likely failed.\nSkipping this project..."
+		continue
 	fi
-
-done <<< "$project_list"
+	rename_and_convert_to_mp3 "$my_render" "$real_filename"
+	echo $real_filename >> /Users/pauldonovan/Tools/reaper-auto-render/FinishedProjects.txt
+done
+echo -e "\n"
